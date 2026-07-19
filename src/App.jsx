d64c05a2bd57e7908b045cfd92f03d16f6ebc06e
Aarti Mohite <aaru_r17@192.168.1.3>
2026-07-19 17:27:30 +0530

    Initial React portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <main>
+      <h1>Hello, I'm Aarati 👋</h1>
+      <p>My React Portfolio is under construction.</p>
+    </main>
+  );
+}
+
+export default App;
